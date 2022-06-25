7f8cf931c08dea0ed00d477c2e8d1c2a8897be30
Trinh V. Nguyen <nvt@thinkpad-t570.hotspot.internet-for-guests.com>
2022-06-25 18:11:24 +0200

    Fix deployment error

--- src/App.svelte
+++ src/App.svelte
@@ -21,7 +21,10 @@
   import { HotelService } from "./services/hotel-service.js";
   import { AttractionService } from "./services/attraction-service.js";
   import { ReverseGeocodingService } from "./services/reverse-geocoding-service";
-  setContext("PlacemarkService", new PlacemarkService("http://localhost:4000"));
+  setContext(
+    "PlacemarkService",
+    new PlacemarkService("https://travisor-hapi.herokuapp.com")
+  );
   setContext("RestaurantService", new RestaurantService());
   setContext("HotelService", new HotelService());
   setContext("AttractionService", new AttractionService());
